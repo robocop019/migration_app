d185aec92cda79860bcb7d4957bc3dc254f3bfa3
robocop019 <chrisbowen@comcast.net>
2019-05-07 16:40:38 -0500

    intial upload

---
+++ db/migrate/20190507165818_make_changes_to_products.rb
@@ -0,0 +1,9 @@
+class MakeChangesToProducts < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :products, :title, :name
+    change_column :products, :description, :text
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+    remove_column :products, :isbn, :string
+  end
+end
